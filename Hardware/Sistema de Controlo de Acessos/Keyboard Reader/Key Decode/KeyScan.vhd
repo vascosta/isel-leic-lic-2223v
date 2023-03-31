7de1e4fe1c6bc7befb62457227c665e6b22df579
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-03-31 15:16:59 +0100

    Update KeyScan.vhd

--- Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/KeyScan.vhd
+++ Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/KeyScan.vhd
@@ -4,15 +4,15 @@ use ieee.std_logic_1164.all;
 entity KeyScan is
 	port
 	(  
-		 -- Input ports
-       Kscan    		: in std_logic;
-		 CLK_in	  		: in std_logic;
-		 ButtonLine		: in std_logic_vector(3 downto 0);
+		-- Input ports
+		Kscan    	: in std_logic;
+		Clk		: in std_logic;
+		ButtonLine	: in std_logic_vector(3 downto 0);
 
-		 -- Output ports
-       K        		: out std_logic_vector(3 downto 0);
-       Kpress   		: out std_logic;
-		 ButtonColumn	: out std_logic_vector(2 downto 0)
+		-- Output ports
+      		K        	: out std_logic_vector(3 downto 0);
+      		Kpress   	: out std_logic;
+		ButtonColumn	: out std_logic_vector(2 downto 0)
 	);
 end KeyScan;
 
@@ -21,25 +21,25 @@ architecture structural of KeyScan is
 component Counter is 
 	port 
 	(
-        -- Input ports
-        CLK : in std_logic;
-        CE	: in std_logic;
+		-- Input ports
+		Clk	: in std_logic;
+     		Ce	: in std_logic;
 
-        -- Output ports
-        O   : out std_logic_vector(3 downto 0)
-    );
+		-- Output ports
+      		O   	: out std_logic_vector(3 downto 0)
+    	);
 end component;
 
 component Mux is 
-    port
-    (
-        -- Input ports
-        I	: in std_logic_vector(3 downto 0);
-        S	: in std_logic_vector(1 downto 0);
+	port
+   	(
+		-- Input ports
+		I	: in std_logic_vector(3 downto 0);
+      		S	: in std_logic_vector(1 downto 0);
 
-        -- Output ports
-        O	: out std_logic
-    );
+      		-- Output ports
+      		O	: out std_logic
+   	);
 end component;
 
 component Decoder is 
@@ -54,34 +54,37 @@ component Decoder is
 end component;
 
 
-signal OCounterX, BLX	: std_logic_vector(3 downto 0);
-signal BCX					: std_logic_vector(2 downto 0);
-signal OMuxX 		: std_logic;
+signal OCounter_X, BL_X	: std_logic_vector(3 downto 0);
+signal BC_X		: std_logic_vector(2 downto 0);
+signal OMux_X 		: std_logic;
 
 begin
 
-K(3) <= OCounterX(3);
-K(2) <= OCounterX(2);
-K(1) <= OCounterX(1);
-K(0) <= OCounterX(0);
+K(3) <= OCounter_X(3);
+K(2) <= OCounter_X(2);
+K(1) <= OCounter_X(1);
+K(0) <= OCounter_X(0);
 
-Kpress <= not OMuxX;
+Kpress <= not OMux_X;
 
-ButtonColumn(0) <= not BCX(0);
-ButtonColumn(1) <= not BCX(1);
-ButtonColumn(2) <= not BCX(2);
+ButtonColumn(0) <= not BC_X(0);
+ButtonColumn(1) <= not BC_X(1);
+ButtonColumn(2) <= not BC_X(2);
 
-BLX(0) <= ButtonLine(0);
-BLX(1) <= ButtonLine(1);
-BLX(2) <= ButtonLine(2);
-BLX(3) <= ButtonLine(3);
+BL_X(0) <= ButtonLine(0);
+BL_X(1) <= ButtonLine(1);
+BL_X(2) <= ButtonLine(2);
+BL_X(3) <= ButtonLine(3);
 
-T1: Counter	port map (CLK => CLK_in , CE => Kscan, O(3) => OCounterX(3), O(2) => OCounterX(2), O(1) => OCounterX(1), O(0) => OCounterX(0));
+T1: Counter	port map (Clk => Clk , Ce => Kscan, 
+			  O(3) => OCounter_X(3), O(2) => OCounter_X(2), O(1) => OCounter_X(1), O(0) => OCounter_X(0));
 								
-T2: Mux 		port map (I(3) => BLX(3), I(2) => BLX(2), I(1) => BLX(1), I(0) => BLX(0), 
-								S(1) => OCounterX(1), S(0) => OCounterX(0), O => OMuxX);
+T2: Mux		port map (I(3) => BL_X(3), I(2) => BL_X(2), I(1) => BL_X(1), I(0) => BL_X(0), 
+			  S(1) => OCounter_X(1), S(0) => OCounter_X(0), 
+			  O => OMux_X);
 			
-T3: Decoder port map (I(1) => OCounterX(3), I(0) => OCounterX(2), O(2) => BCX(2), O(1) => BCX(1), O(0) => BCX(0));
+T3: Decoder	port map (I(1) => OCounter_X(3), I(0) => OCounter_X(2), 
+		   	  O(2) => BC_X(2), O(1) => BC_X(1), O(0) => BC_X(0));
 									
 
 end structural;
