8ade3b6397a99b4c9199ea596325777f06dfb9b7
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-03-31 15:07:28 +0100

    Update Counter.vhd

--- Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Counter.vhd
+++ Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Counter.vhd
@@ -1,22 +1,25 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity Counter is 
 	port 
 	(
 		-- Input ports
 		Clk : in std_logic;
-      		Ce  : in std_logic;
+		Ce  : in std_logic;
 
       		-- Output ports
       		O   : out std_logic_vector(3 downto 0)
-    );
+    	);
 end Counter;
 
 architecture behavioral of Counter is
-    signal count: integer := 0;
+
+signal count: integer := 0;
+	 
 begin
+
     process(Clk)
     begin
         if rising_edge(Clk) then
