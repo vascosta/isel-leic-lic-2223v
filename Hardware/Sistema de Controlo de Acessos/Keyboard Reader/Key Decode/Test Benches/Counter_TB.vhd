e11e053cefbf5c695cc0124420830639a22b083d
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-03-31 10:05:09 +0100

    Update Counter_TB.vhd

--- Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/Counter_TB.vhd
+++ Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/Counter_TB.vhd
@@ -4,14 +4,14 @@ USE ieee.std_logic_1164.all;
 entity Counter_TB is 
 end Counter_TB;
 
-architecture behavioral of COUNTER_TB is
+architecture behavioral of Counter_TB is
 
 component Counter is 
 	port
 	(
 		-- Input ports
-		CLK : in std_logic;
-		CE	 : in std_logic;
+		Clk : in std_logic;
+		Ce	 : in std_logic;
 		
 		-- Output ports
 		O : out std_logic_vector(3 downto 0)
@@ -22,24 +22,24 @@ end component;
 constant MCLK_PERIOD : time := 20 ns;
 constant MCLK_HALF_PERIOD : time := MCLK_PERIOD /2 ;
 
-signal CLK_tb, CE_TB: std_logic;
+signal Clk_tb, Ce_TB: std_logic;
 signal O_TB: std_logic_vector (3 downto 0);
 
 begin
 
 -- UNIT UNDER TEST
-UUT: Counter port map (CLK => CLK_TB, CE => CE_TB,
-								O => O_TB);
+UUT: Counter port map (Clk => Clk_TB, Ce => Ce_TB,
+							  O => O_TB);
 
 clk_gen : process 
 
 begin
 
-CLK_TB <= '0';
+Clk_TB <= '0';
 
 wait for MCLK_HALF_PERIOD;
 
-CLK_TB <= '1';
+Clk_TB <= '1';
 
 wait for MCLK_HALF_PERIOD; 
 
@@ -50,11 +50,11 @@ stimulus : process
 begin
 
 -- reset
-CE_TB <= '0';
+Ce_TB <= '0';
 
 wait for MCLK_PERIOD;
 
-CE_TB <= '1';
+Ce_TB <= '1';
 
 wait for MCLK_PERIOD;
 wait for MCLK_PERIOD;
@@ -73,7 +73,7 @@ wait for MCLK_PERIOD;
 wait for MCLK_PERIOD;
 wait for MCLK_PERIOD;
 
-CE_tb <= '0';
+Ce_TB <= '0';
 
 wait;
 
