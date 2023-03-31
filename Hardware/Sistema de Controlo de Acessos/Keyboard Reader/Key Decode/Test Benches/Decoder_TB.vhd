9b46ec25111f694e6d20864e6c2ea3c5e8a6cd90
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-03-31 15:23:04 +0100

    Update Decoder_TB.vhd

--- Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/Decoder_TB.vhd
+++ Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/Decoder_TB.vhd
@@ -1,5 +1,5 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity Decoder_TB is 
 end Decoder_TB;
@@ -18,8 +18,8 @@ component Decoder is
 end component;
 
 --UUT signals
-constant MCLK_PERIOD : time := 20 ns;
-constant MCLK_HALF_PERIOD : time := MCLK_PERIOD /2 ;
+constant MCLK_PERIOD 		: time := 20 ns;
+constant MCLK_HALF_PERIOD	: time := MCLK_PERIOD /2 ;
 
 signal I_TB: std_logic_vector (1 downto 0);
 signal O_TB: std_logic_vector (2 downto 0);
@@ -27,22 +27,22 @@ signal O_TB: std_logic_vector (2 downto 0);
 begin
 
 -- UNIT UNDER TEST
-UUT: Decoder port map (I => I_tb, O => O_TB);
+UUT: Decoder port map (I => I_TB, O => O_TB);
 
 stimulus : process
 begin
 
-I_tb <= "00";
+I_TB <= "00";
 
 wait for MCLK_PERIOD;
 wait for MCLK_PERIOD;
 
-I_tb <= "01";
+I_TB <= "01";
 
 wait for MCLK_PERIOD;
 wait for MCLK_PERIOD;
 
-I_tb <= "10";
+I_TB <= "10";
 
 wait for MCLK_PERIOD;
 
