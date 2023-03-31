1541efb8a48c0db8605fbfb92aab77f5c5018226
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-03-31 15:24:01 +0100

    Update KeyControl_TB.vhd

--- Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/KeyControl_TB.vhd
+++ Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/KeyControl_TB.vhd
@@ -1,5 +1,5 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity KeyControl_TB is 
 end KeyControl_TB;
@@ -7,83 +7,80 @@ end KeyControl_TB;
 architecture behavioral of KeyControl_TB is
 
 component KeyControl is 
-port(
-         -- Input ports
-        Kpress		: in std_logic;
-        Kack      : in std_logic;
-        clk       : in std_logic;
-        reset    	: in std_logic;
+	port
+	(
+		-- Input ports
+     		Kpress	: in std_logic;
+      		Kack	: in std_logic;
+      		Clk	: in std_logic;
+      		Reset  	: in std_logic;
 
-        -- Output ports
-        Kscan     : out std_logic;
-        Kval      : out std_logic
+      		-- Output ports
+      		Kscan   : out std_logic;
+      		Kval    : out std_logic
 
-);
+	);
 end component;
 
 --UUT signals
-constant MCLK_PERIOD : time := 20 ns;
-constant MCLK_HALF_PERIOD : time := MCLK_PERIOD /2 ;
+constant MClk_PERIOD 		: time := 20 ns;
+constant MClk_HALF_PERIOD	: time := MClk_PERIOD /2 ;
 
-signal Kpress_TB, Kack_TB, clk_TB, reset_TB, Ksacn_TB, Kval_TB: std_logic;
+signal Kpress_TB, Kack_TB, Clk_TB, Reset_TB, Ksacn_TB, Kval_TB: std_logic;
 
 begin
 
 -- UNIT UNDER TEST
-UUT: KeyControl port map (Kpress => Kpress_TB, clk => clk_TB, Kack => Kack_TB, reset => reset_TB, 
-									Kscan => Ksacn_TB, Kval => Kval_TB);
+UUT: KeyControl port map (Kpress => Kpress_TB, Kack => Kack_TB, Clk => Clk_TB, Reset => Reset_TB, 
+			  Kscan => Ksacn_TB, Kval => Kval_TB);
 
-clk_gen : process 
+Clk_gen : process 
 
 begin
 
-clk_tb <= '0';
+Clk_tb <= '0';
 
-wait for MCLK_HALF_PERIOD;
+wait for MClk_HALF_PERIOD;
 
-clk_tb <= '1';
+Clk_tb <= '1';
 
-wait for MCLK_HALF_PERIOD;
+wait for MClk_HALF_PERIOD;
 
 end process;
 
 stimulus : process
 begin
 
--- reset
-reset_TB <= '1';
+-- Reset
+Reset_TB <= '1';
 Kpress_TB <= '0';
 Kack_TB <= '0';
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-reset_TB <= '0';
+Reset_TB <= '0';
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
 Kpress_TB <= '1';
 
-wait for MCLK_PERIOD;
-
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
+wait for MClk_PERIOD;
 
 Kack_TB <= '0';
 
-wait for MCLK_PERIOD;
-
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
+wait for MClk_PERIOD;
 
 Kack_TB <= '1';
 
-wait for MCLK_PERIOD;
-
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
+wait for MClk_PERIOD;
 
 Kpress_TB <= '0';
 
-wait for MCLK_PERIOD;
-
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
+wait for MClk_PERIOD;
 
 wait;
 
