3660fe1df9b95bdd6f6afe6af95efaf648bfe9e5
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-03-31 15:25:51 +0100

    Update KeyDecode_TB.vhd

--- Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/KeyDecode_TB.vhd
+++ Hardware/Sistema de Controlo de Acessos/Keyboard Reader/Key Decode/Test Benches/KeyDecode_TB.vhd
@@ -1,5 +1,5 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity KeyDecode_TB is 
 end KeyDecode_TB;
@@ -10,97 +10,104 @@ component KeyDecode is
 	port
 	(
 		-- Input ports
-		Kack      		: in std_logic;
-		clk    	 		: in std_logic;
-		reset     		: in std_logic;
+		Kack   		: in std_logic;
+		Clk    	 	: in std_logic;
+		reset     	: in std_logic;
 		ButtonLine  	: in std_logic_vector(3 downto 0);
 
 		-- Output ports
-		Kval      		: out std_logic;
-		K	    			: out std_logic_vector(3 downto 0);
-		ButtonColumn  	: out std_logic_vector(2 downto 0)
-);
+		Kval    	: out std_logic;
+		K		: out std_logic_vector(3 downto 0);
+		ButtonColumn	: out std_logic_vector(2 downto 0)
+	);
 end component;
 
 --UUT signals
-constant MCLK_PERIOD : time := 20 ns;
-constant MCLK_HALF_PERIOD : time := MCLK_PERIOD /2 ;
+constant MClk_PERIOD : time := 20 ns;
+constant MClk_HALF_PERIOD : time := MClk_PERIOD /2 ;
 
-signal Kack_tb, clk_tb, reset_tb, Kval_TB: std_logic;
-signal ButtonLine_tb, Kexit_tb: std_logic_vector (3 downto 0);
-signal ButtonColumn_TB: std_logic_vector (2 downto 0);
+signal Kack_TB, Clk_TB, reset_TB, Kval_TB	: std_logic;
+signal ButtonLine_TB, K_TB			: std_logic_vector (3 downto 0);
+signal ButtonColumn_TB				: std_logic_vector (2 downto 0);
 
 begin
 
  --UNIT UNDER TEST
-UUT: KeyDecode port map (CLK => CLK_tb, reset => reset_tb, Kack => Kack_tb,
- Kval => Kval_tb, ButtonLine => ButtonLine_tb, K => Kexit_tb, ButtonColumn => ButtonColumn_TB);
+UUT: KeyDecode port map (Kack => Kack_TB, Clk => Clk_TB, reset => reset_TB, ButtonLine => ButtonLine_TB,
+			 Kval => Kval_TB,  K => K_TB, ButtonColumn => ButtonColumn_TB);
 
-clk_gen : process 
+Clk_gen : process 
+
 begin
-clk_tb <= '0';
-wait for MCLK_HALF_PERIOD;
-clk_tb <= '1';
-wait for MCLK_HALF_PERIOD;
+
+Clk_TB <= '0';
+
+wait for MClk_HALF_PERIOD;
+
+Clk_TB <= '1';
+
+wait for MClk_HALF_PERIOD;
+
 end process;
 
 stimulus : process
+
 begin
 -- reset
 
-reset_tb <= '1';
-Kack_tb <= '1';
+reset_TB <= '1';
+Kack_TB  <= '1';
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0000";
-reset_tb <= '0';
+ButtonLine_TB <= "0000";
+reset_TB <= '0';
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0100";
+ButtonLine_TB <= "0100";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "1000";
+ButtonLine_TB <= "1000";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0001";
+ButtonLine_TB <= "0001";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0101";
+ButtonLine_TB <= "0101";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "1001";
+ButtonLine_TB <= "1001";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0010";
+ButtonLine_TB <= "0010";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0110";
+ButtonLine_TB <= "0110";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "1010";
+ButtonLine_TB <= "1010";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0011";
+ButtonLine_TB <= "0011";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "0111";
+ButtonLine_TB <= "0111";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
-ButtonLine_tb <= "1011";
+ButtonLine_TB <= "1011";
 
-wait for MCLK_PERIOD;
+wait for MClk_PERIOD;
 
 wait;
 
