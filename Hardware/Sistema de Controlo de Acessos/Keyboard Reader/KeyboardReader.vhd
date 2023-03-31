ee41ffe82c8c162729f9feb212d403c5b8b87477
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-03-31 15:02:52 +0100

    Update KeyboardReader.vhd

--- Hardware/Sistema de Controlo de Acessos/Keyboard Reader/KeyboardReader.vhd
+++ Hardware/Sistema de Controlo de Acessos/Keyboard Reader/KeyboardReader.vhd
@@ -14,7 +14,7 @@ entity KeyboardReader is
 		Kval      		: out std_logic;
 		K	    			: out std_logic_vector(3 downto 0);
 		ButtonColumn  	: out std_logic_vector(2 downto 0)
-);
+	);
 end KeyboardReader;
 
 architecture structural of KeyboardReader is
@@ -32,14 +32,29 @@ component KeyDecode is
 		Kval      		: out std_logic;
 		K	    			: out std_logic_vector(3 downto 0);
 		ButtonColumn  	: out std_logic_vector(2 downto 0)
-);
+	);
 end component;
 
+component ClkDiv is 
+	generic (div: natural := 50000000);
+
+	port 
+	( 
+		clk_in	: in std_logic;
+	
+		clk_out	: out std_logic
+	);
+
+end component;
+
+signal Clk_X : std_logic;
 
 begin
 
-F1: KeyDecode 	port map(Kack => Kack, clk => clk, reset => reset, ButtonLine => ButtonLine, 
+F1: KeyDecode 	port map(Kack => Kack, clk => Clk_X, reset => reset, ButtonLine => ButtonLine, 
 									Kval => Kval, K => K, ButtonColumn => ButtonColumn);
+									
+F3: ClkDiv  	generic map(1000) port map (clk_in => clk, clk_out => Clk_X);						
 
 end structural;
 
